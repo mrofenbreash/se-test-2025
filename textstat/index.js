a6a504d7145e669d454022d28a7ed7401add0bff
em <em@mail.ru>
2025-06-18 16:25:27 +0300

    2nd task

--- textstat/index.js
+++ textstat/index.js
@@ -8,8 +8,29 @@
  * @param {object} event - Event class
  */
 function onStatisticsClicked(event) {
-    //TODO: Implement this function
-    console.log('onStatisticsClicked called');
+    const text = document.getElementById('text').value;
+    
+    // total number of words
+    const totalChars = text.length;
+    
+    // number of spaces
+    const spaces = (text.match(/ /g) || []).length;
+    
+    // number of_glasnie (a, e, i, o, u, another_registry)
+    const vowels = (text.match(/[aeiouAEIOU]/g) || []).length;
+    
+    // number of_soglasnie (except: glasnie)
+    const consonants = (text.match(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/g) || []).length;
+    
+    // number of words
+    const words = text.trim().split(/\s+/).filter(word => word.length > 0).length;
+    
+    // refresh fields
+    document.getElementById('valCharCount').value = totalChars;
+    document.getElementById('valSpacesCount').value = spaces;
+    document.getElementById('valVowelsCount').value = vowels;
+    document.getElementById('valConsonantsCount').value = consonants;
+    document.getElementById('valWordsCount').value = words;
 }
 
 /**
@@ -18,9 +39,19 @@ function onStatisticsClicked(event) {
  * @param {object} event - Event class
  */
 function onRemoveWordsClicked(event) {
-    //TODO: Implement this function
-    console.log('onRemoveWordsClicked called');
-}
+    const text = document.getElementById('text').value;
+    
+    // diff page to words (withou space symbols)
+    const words = text.trim().split(/\s+/).filter(word => word.length > 0);
+    
+    // words with (ne4etnie) numbers (1, 3, 5, ...)
+    const oddWords = words.filter((word, index) => index % 2 === 0);
+    
+    // match words back to text
+    const result = oddWords.join(' ');
+    
+    //output, field: "Odd words"
+    document.getElementById('valOddWords').value = result;
 
 
 /**********************************************/
